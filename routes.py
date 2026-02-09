e479e103672a3a48d9cc8f7530742ee524d1661b
khushinikam <khushi.nikam@gammaedge.io>
2026-02-09 17:31:23 +0530

    updated changes

--- routes.py
+++ routes.py
@@ -26,10 +26,12 @@ def signup(user: schemas.userCreate, db: Session = Depends(get_db)):
 
 # users can login with same credentials
 
-@router.post("/login", response_model=schemas.token)
-
 def login(user: schemas.userLogin, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.email == user.email).first()
+
+    if not db_user or not authentication.verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
 
     token = authentication.create_access_token({"subject": db_user.email})
     return {"access_token": token, "token_type": "bearer"}
