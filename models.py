b3df50f8d431a5e28f33646b74173ab1e1eefcc4
khushinikam <khushi.nikam@gammaedge.io>
2026-02-10 12:11:41 +0530

    Implemented Alembiic in project

--- models.py
+++ models.py
@@ -8,3 +8,4 @@ class User(BaseClass):
     username = Column(String, unique=True,nullable=False)
     email = Column(String, unique=True,nullable=False)
     password = Column(String)
+    phoneNumber = Column(String, unique = True, nullable= False)
